97234f926a2bdb65b4c8f0d5349ff0070d2a37f6
Islam Ahmed <142545372+SoftEng-Islam@users.noreply.github.com>
2023-11-14 07:26:04 +0200

    update

--- src/components/DokuList.vue
+++ src/components/DokuList.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 	import { ref } from 'vue';
-	import DokuPages from "./DokuPages.vue";
+	// import DokuPages from "./DokuPages.vue";
 
 	import { useTippy } from 'vue-tippy'
 	const btn = ref()
@@ -28,7 +28,7 @@
 </script>
 <template lang="pug">
 //- All Docs List
-div(class="absolute duration-100 h-[var(--DokuListsHeight)] bg-[var(--LTheme3)] dark:bg-[var(--Theme3)]" :style="DokuListClosed ? 'left: -13rem' : 'left: 0'")
+div(v-show="false" class="absolute duration-100 h-[var(--DokuListsHeight)] bg-[var(--LTheme3)] dark:bg-[var(--Theme3)]" :style="DokuListClosed ? 'left: -13rem' : 'left: 0'")
 	div(class="h-full w-52 p-4 z-10")
 		div()
 			input(type="text", placeholder="Search..." value="" , class="w-full h-8 px-2 rounded-md bg-[var(--LTheme1)] dark:bg-[var(--Theme1)] text-black dark:text-white placeholder:text-black dark:placeholder:text-white" )
@@ -40,7 +40,7 @@ div(class="absolute duration-100 h-[var(--DokuListsHeight)] bg-[var(--LTheme3)] 
 		button(type="button" @click="close" class="w-4 h-11 rounded-tr-lg rounded-br-lg bg-[var(--LTheme3)] dark:bg-[var(--Theme3)] absolute z-20 top-1/2 right-[-1rem] translate-y-[-50%]")
 			<i v-show="ri_arrow_left" class="ri-arrow-left-s-line dark:text-white"></i>
 			<i v-show="ri_arrow_right" class="ri-arrow-right-s-line dark:text-white"></i>
-	<doku-pages />
+	//- <doku-pages />
 </template>
 <style scoped lang="sass">
 .DokuList_Ul
